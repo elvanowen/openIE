ee99b14a4127e55e3ef37ae5b893d8a14c7b5b33
Elvan Owen <vanzz_95@hotmail.com>
2017-03-02 00:27:19 +0700

    Added alert after load plugins

---
+++ src/main/java/id/ac/itb/gui/progressbar/ProgressBar.java
@@ -0,0 +1,7 @@
+package id.ac.itb.gui.progressbar;
+
+/**
+ * Created by elvanowen on 3/2/17.
+ */
+public class ProgressBar {
+}
---
+++ src/main/java/id/ac/itb/util/UnzipUtility.java
@@ -0,0 +1,7 @@
+package id.ac.itb.util;
+
+/**
+ * Created by elvanowen on 3/1/17.
+ */
+public class UnzipUtility {
+}
